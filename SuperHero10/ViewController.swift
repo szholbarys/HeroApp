2766a34e55b7e453795c6af74945d8d3088a1d99
szholbarys <sakenzholbarys@gmail.com>
2024-12-04 22:55:35 +0500

    Initial Commit

---
+++ SuperHero10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperHero10
+//
+//  Created by Zholbarys on 04.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
